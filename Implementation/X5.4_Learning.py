Fixed a number of mistakes in analytical treatment. Now analytical and numerical results fit, Implemented measurement and output of analytic (rescaled) variables from micro model, Implemented collection and output of capital stocks of switching agents to compare with assumptions in analytic approximation.

Implementation/X5.4_Learning.py
@@ -200,7 +200,6 @@ def RUN_FUNC(t_a, phi, alpha,
         # adapt parameters where necessary
 
         # set tau according to t_a and phi
-        input_params['tau'] = t_a/(1.-phi)
         input_params['r_depletion'] = True
         input_params['learning'] = True
 
@@ -209,7 +208,7 @@ def RUN_FUNC(t_a, phi, alpha,
 
     # initializing the model
 
-    m = model.divestment_core(**input_params)
+    m = model.Divestment_Core(**input_params)
 
     if transition and t_a > 10.:
         m.mode = 1
@@ -376,7 +375,7 @@ if no_heuristics:
 set different times for resource depletion
 in units of capital accumulation time t_d = 1/(d_c*(1-kappa_d))
 """
-t_as = [1, 3, 100]
+b_cs = [1, 3, 100]
 
 """
 set array of phis to generate equilibrium conditions for
@@ -394,7 +393,7 @@ dictionary of the variable parameters in this experiment together with their
 position in the index of the dictionary of results
 """
 parameters = {
-        't_a': 0,
+    'b_c': 0,
         'phi': 1,
         'alpha': 2,
         'test': 3}
@@ -424,13 +423,13 @@ Make sure, opinion_presets are not expanded
 print mode
 if mode == 1:  # Production
     PARAM_COMBS = list(itertools.product(
-        t_as, phis, alphas, t_d,
+        b_cs, phis, alphas, t_d,
         [opinion_presets], eps,
         transition, test))
 
 elif mode == 2:  # test
     PARAM_COMBS = list(itertools.product(
-        t_as, phis, alphas, t_d,
+        b_cs, phis, alphas, t_d,
         [opinion_presets], eps,
         transition, test))
 
@@ -438,7 +437,7 @@ elif mode == 3:  # messy
     test = [True]
     phis = [round(x, 2) for x in list(np.linspace(0.0, 1.0, 5))[1:-1]]
     PARAM_COMBS = list(itertools.product(
-        t_as, phis, alpha, t_d,
+        b_cs, phis, alpha, t_d,
         [opinion_presets], eps,
         transition, test))
 elif mode == 4:

Implementation/X6.0_macro_trajectory.py
@@ -12,9 +12,11 @@ from macro_model import integrate_equations as macro_model
 from micro_model import divestment_core as micro_model
 from pymofa.experiment_handling import experiment_handling, \
     even_time_series_spacing
+from divestvisuals.data_visualization import \
+    plot_m_trajectories, plot_trajectories
 
 
-def RUN_FUNC(b_c, phi, approximate, test, filename):
+def RUN_FUNC(b_d, phi, approximate, test, filename):
     """
     Set up the model for various parameters and determine
     which parts of the output are saved where.
@@ -24,8 +26,8 @@ def RUN_FUNC(b_c, phi, approximate, test, filename):
 
     Parameters:
     -----------
-    b_c : float > 0
-        the solow residual in the clean sector
+    b_d : float > 0
+        the solow residual in the dirty sector
     phi : float \in [0,1]
         the rewiring probability for the network update
     approximate: bool
@@ -40,11 +42,12 @@ def RUN_FUNC(b_c, phi, approximate, test, filename):
 
     # Parameters:
 
-    input_params = {'b_c': b_c, 'phi': phi, 'tau': 1,
-                    'eps': 0.05, 'b_d': 1.2, 'e': 100,
-                    'G_0': 30000, 'b_r0': 0.1 ** 2 * 100,
+    input_params = {'b_c': 1., 'phi': phi, 'tau': 1.,
+                    'eps': 0.05, 'b_d': b_d, 'e': 100.,
+                    'b_r0': 0.1 ** 2 * 100.,
                     'possible_opinions': [[0], [1]],
-                    'C': 100, 'xi': 1. / 8., 'beta': 0.06,
+                    'xi': 1. / 8., 'beta': 0.06,
+                    'P': 100., 'C': 100., 'G_0': 800.,
                     'campaign': False, 'learning': True}
 
     # investment_decisions:
@@ -63,18 +66,25 @@ def RUN_FUNC(b_c, phi, approximate, test, filename):
     adjacency_matrix = nx.adj_matrix(net).toarray()
     investment_decisions = np.random.randint(low=0, high=2, size=N)
 
-    clean_investment = np.ones(N)
-    dirty_investment = np.ones(N)
+    clean_investment = np.ones(N) * 50. / float(N)
+    dirty_investment = np.ones(N) * 50. / float(N)
 
     init_conditions = (adjacency_matrix, investment_decisions,
                        clean_investment, dirty_investment)
 
+    # for testing reasons, I saved one set of initial conditions
+    # with open('init.pkl', 'wb') as initfile:
+    #    cp.dump(init_conditions, initfile)
+    if test is True:
+        print 'loading initial conditions'
+        init_conditions = np.load('init.pkl')
+
     # initializing the model
     print 'approximate', approximate
     if approximate:
-        m = macro_model.integrate_equations(*init_conditions, **input_params)
+        m = macro_model.Integrate_Equations(*init_conditions, **input_params)
     elif not approximate:
-        m = micro_model.divestment_core(*init_conditions, **input_params)
+        m = micro_model.Divestment_Core(*init_conditions, **input_params)
 
     # storing initial conditions and parameters
 
@@ -102,26 +112,25 @@ def RUN_FUNC(b_c, phi, approximate, test, filename):
 
     # run the model
     t_start = time.clock()
-    t_max = 200
-    m.R_depletion = False
-    m.run(t_max=t_max)
-    t_max += 200
-    m.R_depletion = True
-    m.run(t_max=t_max)
 
-    # store exit status
+    t_max = 200 if not test else 1
+    print t_max
+    m.R_depletion = False
+    exit_status = m.run(t_max=t_max)
+
+    t_max += 400 if not test else 1
+    print t_max
+    m.R_depletion = True
+    exit_status = m.run(t_max=t_max)
+
     res["runtime"] = time.clock() - t_start
 
     # store data in case of successful run
-
     if exit_status in [0, 1]:
         # interpolate m_trajectory to get evenly spaced time series.
-        df = m.m_trajectory
-        dfo = even_time_series_spacing(df, 101, 0., t_max)
-        res["economic_trajectory"] = dfo
-
-    end = time.clock()
-    res["runtime"] = end - start
+        res["macro_trajectory"] = \
+            even_time_series_spacing(m._get_m_trj(), 201, 0., t_max)
+        res["switchlist"] = m._get_switch_list()
 
     # save data
     with open(filename, 'wb') as dumpfile:
@@ -135,6 +144,9 @@ def RUN_FUNC(b_c, phi, approximate, test, filename):
 
 
 # get sub experiment and mode from command line
+
+# experiment, mode, test
+
 if len(sys.argv) > 1:
     input_int = int(sys.argv[1])
 else:
@@ -142,19 +154,22 @@ else:
 if len(sys.argv) > 2:
     mode = int(sys.argv[2])
 else:
-    mode = None
+    mode = 0
 if len(sys.argv) > 3:
     test = [bool(sys.argv[3])]
 else:
     test = [False]
 
-experiments = ['micro', 'macro', 'short']
+experiments = ['micro', 'macro', 'macroshort', 'microshort']
 sub_experiment = experiments[input_int]
 folder = 'X6' + sub_experiment + 'trajectory'
 
+if test[0] is True:
+    print sub_experiment, mode, test
+
 # check if cluster or local
 if getpass.getuser() == "kolb":
-    SAVE_PATH_RAW = "/P/tmp/kolb/Divest_Experiments/divestdata/" \
+    SAVE_PATH_RAW = "/p/tmp/kolb/Divest_Experiments/divestdata/" \
                     + folder + "/raw_data" + '_' + sub_experiment + '/'
     SAVE_PATH_RES = "/home/kolb/Divest_Experiments/divestdata/" \
                     + folder + "/results" + '_' + sub_experiment + '/'
@@ -166,25 +181,36 @@ elif getpass.getuser() == "jakob":
         "/home/jakob/PhD/Project_Divestment/Implementation/divestdata/" \
         + folder + "/results" + '_' + sub_experiment + '/'
 
-phis = [round(x, 5) for x in list(np.linspace(0.0, 1.0, 11))]
+phis = [round(x, 5) for x in list(np.linspace(0.0, 0.9, 10))]
 
-b_cs = [round(x, 5) for x in list(np.linspace(0.4, 2.0, 9))]
+b_ds = [round(x, 5) for x in list(np.linspace(1., 1.5, 3))]
 
 parameters = {'b_c': 0, 'phi': 1, 'macro_model': 2, 'test': 3}
-b_c, phi, approximate, exact, test = \
-    [1], [.8], [True], [False], [False]
+b_d, phi, approximate, exact = \
+    [1.2], [.8], [True], [False]
 
 NAME = 'b_c_scan_' + sub_experiment + '_trajectory'
 INDEX = {0: "b_c", 1: "phi"}
 
 if sub_experiment == 'micro':
-    PARAM_COMBS = list(it.product(b_cs, phis, exact, test))
+    PARAM_COMBS = list(it.product(b_ds, phis, exact, test))
 
 elif sub_experiment == 'macro':
-    PARAM_COMBS = list(it.product(b_cs, phis, approximate, test))
+    PARAM_COMBS = list(it.product(b_ds, phis, approximate, test))
 
-elif sub_experiment == 'short':
-    PARAM_COMBS = list(it.product(b_c, phi, approximate, test))
+elif sub_experiment == 'macroshort' and test[0] is False:
+    PARAM_COMBS = list(it.product(b_d, phis, approximate, test))
+
+elif sub_experiment == 'microshort' and test[0] is False:
+    PARAM_COMBS = list(it.product(b_d, phis, exact, test))
+
+elif sub_experiment == 'macroshort' and test[0] is True:
+    print '### testing mode ### macro'
+    PARAM_COMBS = list(it.product(b_d, phi, approximate, test))
+
+elif sub_experiment == 'microshort' and test[0] is True:
+    print '### testing mode ### micro'
+    PARAM_COMBS = list(it.product(b_d, [0.2, 0.7, 0.8], exact, test))
 
 else:
     print sub_experiment, ' is not in the list of possible experiments'
@@ -192,36 +218,50 @@ else:
 
 # names and function dictionaries for post processing:
 NAME1 = NAME + '_trajectory'
-EVA1 = {"<mean_trajectory>":
-            lambda fnames: pd.concat([np.load(f)["economic_trajectory"]
+EVA1 = {"mean_trajectory":
+            lambda fnames: pd.concat([np.load(f)["macro_trajectory"]
                                       for f in fnames]).groupby(
                 level=0).mean(),
-        "<sem_trajectory>":
-            lambda fnames: pd.concat([np.load(f)["economic_trajectory"]
-                                      for f in fnames]).groupby(level=0).sem()}
+        "sem_trajectory":
+            lambda fnames: pd.concat([np.load(f)["macro_trajectory"]
+                                      for f in fnames]).groupby(level=0).std()}
+NAME2 = NAME + '_switchlist'
+CF2 = lambda fnames: pd.concat([np.load(f)["switchlist"] \
+                                for f in fnames]).sortlevel(level=0)
+
 # full run
 if mode == 0:
     print 'mode 0'
-    SAMPLE_SIZE = 100 if sub_experiment == 'micro' else 3
+    sys.stdout.flush()
+    SAMPLE_SIZE = 100 if sub_experiment == 'micro' else 2
     handle = experiment_handling(SAMPLE_SIZE, PARAM_COMBS, INDEX,
                                  SAVE_PATH_RAW, SAVE_PATH_RES)
     handle.compute(RUN_FUNC)
     handle.resave(EVA1, NAME1)
+    handle.collect(CF2, NAME2)
 
 # test run
 if mode == 1:
     print 'mode 1'
-    SAMPLE_SIZE = 2 if sub_experiment == 'micro' else 3
+    sys.stdout.flush()
+    SAMPLE_SIZE = 2 if sub_experiment == 'micro' else 2
     handle = experiment_handling(SAMPLE_SIZE, PARAM_COMBS, INDEX,
                                  SAVE_PATH_RAW, SAVE_PATH_RES)
     handle.compute(RUN_FUNC)
     handle.resave(EVA1, NAME1)
 
-# debug and mess around mode:
-if mode is None:
-    print 'mode 2'
-    SAMPLE_SIZE = 2 if sub_experiment == 'micro' else 3
+# debug and mess around mode with plotting:
+if mode is 3:
+    print 'mode 2 - plotting'
+    print 'short is ', test
+    sys.stdout.flush()
+    SAMPLE_SIZE = 2 if sub_experiment == 'microshort' else 2
     handle = experiment_handling(SAMPLE_SIZE, PARAM_COMBS, INDEX,
                                  SAVE_PATH_RAW, SAVE_PATH_RES)
-    handle.compute(RUN_FUNC)
-    handle.resave(EVA1, NAME1)
+    print SAMPLE_SIZE
+    if test[0] is True:
+        handle.compute(RUN_FUNC)
+        handle.resave(EVA1, NAME1)
+        handle.collect(CF2, NAME2)
+        # plot_m_trajectories(SAVE_PATH_RES, NAME1)
+        #plot_trajectories(SAVE_PATH_RES, NAME1, None, None)