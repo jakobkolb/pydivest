Fixed a number of mistakes in analytical treatment. Now analytical and numerical results fit, Implemented measurement and output of analytic (rescaled) variables from micro model, Implemented collection and output of capital stocks of switching agents to compare with assumptions in analytic approximation.

Implementation/X5.6_Phase_Transition.py
@@ -37,7 +37,8 @@ Just for the fun of it, I will check the pure adaptive
 voter case against the case with heuristic decision making.
 """
 
-from pymofa.experiment_handling import experiment_handling
+from pymofa.experiment_handling import \
+    experiment_handling, even_time_series_spacing
 from micro_model import divestment_core as model
 from divestvisuals.data_visualization import plot_phase_transition
 import numpy as np
@@ -101,11 +102,11 @@ def RUN_FUNC(phi, N, alpha,
     # P = 0.125  # question: SHOULDN'T THE MEAN DEGREE STAY CONSTANT?? Yes!
     p = 10. / N
     tau = 1.
-    P = 500
+    P = float(N) * 10
     b_d = 1.2
-    b_c = 0.4
+    b_c = 1.
     d_c = 0.06
-    e = 100
+    e = 100.
     s = 0.23
     t_g = 100.
 
@@ -126,8 +127,7 @@ def RUN_FUNC(phi, N, alpha,
         # set t_max for run in units of the social
         # equilibration time (since this is the
         # process that actually has to equilibrate
-        t_a = tau / (1 - phi)
-        t_max = 100 * t_a
+        t_max = 100
 
         # building initial conditions
 
@@ -145,7 +145,7 @@ def RUN_FUNC(phi, N, alpha,
         # input parameters
 
         input_params = {'adjacency': adjacency_matrix,
-                        'investment_decisions': opinions,
+                        'opinions': opinions,
                         'investment_clean': investment_clean,
                         'investment_dirty': investment_dirty,
                         'possible_opinions': possible_opinions,
@@ -153,7 +153,7 @@ def RUN_FUNC(phi, N, alpha,
                         'P': P, 'b_d': b_d, 'b_c': b_c,
                         'b_r0': b_R0, 'G_0': G_0,
                         'e': e, 'd_c': d_c, 'test': bool(test),
-                        'R_depletion': transition}
+                        'R_depletion': transition, 'learning': True}
 
     # ROUND TWO: TRANSITION
     elif transition:
@@ -170,7 +170,6 @@ def RUN_FUNC(phi, N, alpha,
                 + '*_final')
         init_files = glob.glob(path)
         init_file = init_files[np.random.randint(0, len(init_files))]
-        print init_file
         input_params = np.load(init_file)['final_state']
 
         # adapt parameters where necessary
@@ -179,16 +178,12 @@ def RUN_FUNC(phi, N, alpha,
         input_params['R_depletion'] = True
 
         # set t_max for run
-        t_a = tau / (1 - phi)
-        t_max = 300 * t_a
+        t_max = 200
 
     # initializing the model
 
-    m = model.divestment_core(**input_params)
+    m = model.Divestment_Core(**input_params)
 
-    # set to run until convergence only
-
-    m.run_full_time = False
 
     # storing initial conditions and parameters
 
@@ -228,6 +223,11 @@ def RUN_FUNC(phi, N, alpha,
     res["remaining_resource"] = m.G
     res["remaining_resource_fraction"] = m.convergence_state
     res["majority_time"] = m.convergence_time
+
+    res["e_trajectory"] = \
+        even_time_series_spacing(m._get_e_trj(), 201, 0., t_max)
+    res["m_trajectory"] = \
+        even_time_series_spacing(m._get_m_trj(), 201, 0., t_max)
 
     # store runtime:
 
@@ -285,7 +285,7 @@ set path variables according to local of cluster environment
 """
 if getpass.getuser() == "kolb":
     SAVE_PATH_RAW = \
-        "/P/tmp/kolb/Divest_Experiments/divestdata/" \
+        "/p/tmp/kolb/Divest_Experiments/divestdata/" \
         + folder + "/raw_data"
     SAVE_PATH_RES = \
         "/home/kolb/Divest_Experiments/divestdata/" \
@@ -302,7 +302,7 @@ set path variable for initial conditions for transition runs
 """
 if getpass.getuser() == "kolb":
     SAVE_PATH_INIT = \
-        "/P/tmp/kolb/Divest_Experiments/divestdata/" \
+        "/p/tmp/kolb/Divest_Experiments/divestdata/" \
         + FOLDER_EQUI + "/raw_data"
 elif getpass.getuser() == "jakob":
     SAVE_PATH_INIT = \
@@ -332,7 +332,7 @@ if no_heuristics:
 """
 set array of fractions of rewiring events phi
 """
-phis = [round(x, 2) for x in list(np.linspace(0.0, 1.0, 101))[:-1]]
+phis = [round(x, 2) for x in list(np.linspace(0.0, 1.0, 21))[:-1]]
 
 """
 set array of numbers of households N
@@ -408,27 +408,53 @@ def foo(fnames):
     return 1.
 
 
+NAME1 = NAME + "_e_trajectory"
+EVA1 = {"mean_trajectory":
+            lambda fnames: pd.concat([np.load(f)["e_trajectory"]
+                                      for f in fnames]).groupby(
+                level=0).mean(),
+        "sem_trajectory":
+            lambda fnames: pd.concat([np.load(f)["economic_trajectory"]
+                                      for f in fnames]).groupby(level=0).sem(),
+        "min_trajectory":
+            lambda fnames: pd.concat([np.load(f)["economic_trajectory"]
+                                      for f in
+                                      fnames]).groupby(level=0).min(),
+        "max_trajectory":
+            lambda fnames: pd.concat([np.load(f)["economic_trajectory"]
+                                      for f in
+                                      fnames]).groupby(level=0).max()
+        }
+
 NAME2 = NAME + '_convergence'
-EVA2 = {"<mean_remaining_resource_fraction>":
+EVA2 = {"mean_remaining_resource_fraction":
             lambda fnames: np.nanmean(
                 [np.load(f)["remaining_resource_fraction"]
                  for f in fnames]),
-        "<mean_majority_time>":
+        "sem_remaining_resource_fraction":
+            lambda fnames: st.sem(
+                [np.load(f)["remaining_resource_fraction"]
+                 for f in fnames]),
+        "mean_remaining_resource":
+            lambda fnames: np.nanmean(
+                [np.load(f)["remaining_resource"]
+                 for f in fnames]),
+        "mean_majority_time":
             lambda fnames: np.nanmean([np.load(f)["majority_time"]
                                        for f in fnames]),
-        "<min_majority_time>":
+        "min_majority_time":
             lambda fnames: np.nanmin([np.load(f)["majority_time"]
                                       for f in fnames]),
-        "<max_majority_time>":
+        "max_majority_time":
             lambda fnames: np.max([np.load(f)["majority_time"]
                                    for f in fnames]),
-        "<nanmax_majority_time>":
+        "nanmax_majority_time":
             lambda fnames: np.nanmax([np.load(f)["majority_time"]
                                       for f in fnames]),
-        "<sem_majority_time>":
+        "sem_majority_time":
             lambda fnames: st.sem([np.load(f)["majority_time"]
                                    for f in fnames]),
-        "<runtime>":
+        "runtime":
             lambda fnames: st.sem([np.load(f)["runtime"]
                                    for f in fnames]),
         }
@@ -448,18 +474,23 @@ if mode == 1:
 
 # test run
 if mode == 2:
-    SAMPLE_SIZE = 3
+    SAMPLE_SIZE = 10
+    if not transition:
+        SAMPLE_SIZE = 5
     handle = experiment_handling(
         SAMPLE_SIZE, PARAM_COMBS, INDEX, SAVE_PATH_RAW, SAVE_PATH_RES)
     handle.compute(RUN_FUNC)
+    handle.resave(EVA1, NAME1)
     handle.resave(EVA2, NAME2)
+    if transition[0]:
+        plot_phase_transition(SAVE_PATH_RES, NAME2)
 
 # debug and mess around mode:
 if mode == 3:
     SAMPLE_SIZE = 3
     handle = experiment_handling(
         SAMPLE_SIZE, PARAM_COMBS, INDEX, SAVE_PATH_RAW, SAVE_PATH_RES)
-    handle.compute(RUN_FUNC)
+    # handle.compute(RUN_FUNC)
     if transition[0]:
-        handle.resave(EVA2, NAME2)
-        # plot_phase_transition(SAVE_PATH_RES, NAME2)
+        # handle.resave(EVA2, NAME2)
+        plot_phase_transition(SAVE_PATH_RES, NAME2)

Implementation/divestvisuals/data_visualization.py
@@ -176,6 +176,11 @@ def plot_network(loc):
     fig2.savefig(loc+'_network_plot')
 
 
+def plot_m_trajectories(loc, name):
+    with open(loc + name) as target:
+        tmp = np.load(target)
+
+
 def plot_trajectories(loc, name, params, indices):
 
     print 'plotting trajectories'
@@ -183,6 +188,8 @@ def plot_trajectories(loc, name, params, indices):
     with open(loc+name) as target:
         tmp = np.load(target).replace([np.inf, -np.inf], np.nan)
     dataframe = tmp.where(tmp < 10**300, np.nan)
+    print dataframe.columns
+    print dataframe.index.levels[2]
 
     # get the values of the first two index levels
     ivals = dataframe.index.levels[0]
@@ -202,37 +209,34 @@ def plot_trajectories(loc, name, params, indices):
 
             # extract the mean and std data for each of the values of the first index
             subset = dataframe.loc[(ival, jval,),
-                    '<mean_trajectory>'].unstack('observables').dropna(axis=0, how='any')
+                                   'mean_trajectory'].unstack(
+                'observables').dropna(axis=0, how='any')
             subset_errors = dataframe.loc[(ival, jval,),
-                    '<sem_trajectory>'].unstack('observables').dropna(axis=0, how='any')
+                                          'sem_trajectory'].unstack(
+                'observables').dropna(axis=0, how='any')
             for c, column in enumerate(columns):
 
-                # take absolute values of data but save intervals on which original 
-                # data was negative (paint them afterwards)
-                subset_j = subset.loc[1:, column]
-                subset_j_errors = subset_errors.loc[1:, column]
-                subset_abs = subset_j.abs()
-                paint = subset_j.where(subset_j<0).dropna().index.values
+                subset_j = subset.loc[:, column]
+                subset_j_errors = subset_errors.loc[:, column]
 
                 # add a subplot to the list of axes
                 axes.append(plt.subplot2grid((len(columns), len(ivals)), (c, i)))
                 # plot mean e_trajectory and insequrity interval
                 # also mark the area in which the mean data was negative
-                subset_abs.plot(ax = axes[-1])
-                plt.fill_between(subset_abs.index, 
-                        subset_abs - subset_j_errors,
-                        subset_abs + subset_j_errors,
-                        where = subset_abs - subset_j_errors > 0,
+                subset_j.plot(ax=axes[-1])
+                plt.fill_between(subset_j.index,
+                                 subset_j - subset_j_errors,
+                                 subset_j + subset_j_errors,
                         color='blue',
                         alpha=0.1)
-                (subset_abs - subset_j_errors).plot(ax = axes[-1], color='blue', alpha=0.3)
-                (subset_abs + subset_j_errors).plot(ax = axes[-1], color='blue', alpha=0.3)
-                if len(paint)>0:
-                    plt.axvspan(paint[0], paint[-1], alpha=0.2, color = 'red')
+                (subset_j - subset_j_errors).plot(ax=axes[-1], color='blue',
+                                                  alpha=0.3)
+                (subset_j + subset_j_errors).plot(ax=axes[-1], color='blue',
+                                                  alpha=0.3)
 
                 # change y axis scalling to 'log' for plots with nonzero data
-                if sum(np.sign(subset_abs))>0:
-                    axes[-1].set_yscale('log', nonposy='mask')
+                # if sum(np.sign(subset_j))>0:
+                #     axes[-1].set_yscale('log', nonposy='mask')
 
                 # adjust locator counts and add y axis label
                 plt.locator_params(axis='x', tight=True, nbins=5)
@@ -241,6 +245,7 @@ def plot_trajectories(loc, name, params, indices):
                     axes[-1].xaxis.set_visible(False)
 
         # adjust the grid layout to avoid overlapping plots and save the figure
+        print 'saving figure' + `round(jval, 4)`
         fig.tight_layout()
         fig.savefig(loc+'testfigure_'+`round(jval,4)`+'.pdf')
         fig.clf()
@@ -588,8 +593,11 @@ def plot_phase_transition(loc, name, ):
 
     with open(loc + name) as target:
         tmp = np.load(target)
-
-    print tmp
+    tmp = tmp.xs(0.5, level='alpha')[['<mean_remaining_resource_fraction>']]
+    print tmp.index.names
+    print list(tmp.index.levels[1])
+    tmp.unstack('N').plot()
+    plt.show()
 
 if __name__ == '__main__':
 	loc = sys.argv[1]

Implementation/macro_model/integrate_equations.py
@@ -6,13 +6,13 @@ import sympy as sp
 import pandas as pd
 
 
-class integrate_equations:
+class Integrate_Equations:
     def __init__(self, adjacency=None, investment_decisions=None,
                  investment_clean=None, investment_dirty=None,
                  possible_opinions=None,
                  tau=0.8, phi=.7, eps=0.05,
-                 P=1000., r_b=0, b_c=1., b_d=1.5, s=0.23, d_c=0.06,
-                 b_r0=1., e=10, G_0=1000,
+                 P=100., r_b=0, b_c=1., b_d=1.5, s=0.23, d_c=0.06,
+                 b_r0=1., e=10, G_0=3000,
                  R_depletion=True, test=False,
                  C=1, beta=0.06, xi=1. / 8., learning=False,
                  campaign=False):
@@ -20,14 +20,14 @@ class integrate_equations:
         # Social parameters
 
         # mean waiting time between social updates
-        self.tau = tau
+        self.tau = float(tau)
         # rewiring probability for adaptive voter model
-        self.phi = phi
+        self.phi = float(phi)
         # percentage of rewiring and imitation events that are noise
-        self.eps = eps
+        self.eps = float(eps)
         # number of households (to interface with initial
         # conditions from micro model)
-        self.n = adjacency.shape[0]
+        self.n = float(adjacency.shape[0])
         # edges/nodes
         self.k = float(sum(sum(adjacency))) / self.n
         # investment_decisions as indices of possible_opinions
@@ -36,21 +36,21 @@ class integrate_equations:
         # Sector parameters
 
         # Clean capital depreciation rate
-        self.d_c = d_c
+        self.d_c = float(d_c)
         # Dirty capital depreciation rate
-        self.d_d = self.d_c
+        self.d_d = float(self.d_c)
         # knowledge depreciation rate
-        self.beta = self.d_c
+        self.beta = float(self.d_c)
         # Resource harvest cost per unit (at full resource stock)
-        self.b_r0 = b_r0
+        self.b_r0 = float(b_r0)
         # percentage of income saved
-        self.s = s
+        self.s = float(s)
         # solow residual for clean sector
-        self.b_c = b_c
+        self.b_c = float(b_c)
         # solow residual for dirty sector
-        self.b_d = b_d
+        self.b_d = float(b_d)
         # elasticity of knowledge
-        self.xi = xi
+        self.xi = float(xi)
         # labor elasticity (equal in both sectors)
         self.pi = 1. / 2.
         # clean capital elasticity
@@ -58,22 +58,22 @@ class integrate_equations:
         # dirty capital elasticity
         self.kappa_d = 1. - self.pi
         # fossil->energy->output conversion efficiency (Leontief)
-        self.e = e
+        self.e = float(e)
         # total labor
-        self.P = P
+        self.P = float(P)
         # labor per household
         self.p = float(P) / self.n
         # total knowledge stock
-        self.C = C
+        self.C = float(C)
         # unprofitable fraction of fossil reserve
         self.alpha = (b_r0 / e) ** 0.5
 
         # Ecosystem parameters
 
         # initial fossil resource stock
-        self.G_0 = G_0
+        self.G_0 = float(G_0)
         # total fossil resource stock
-        self.G = G_0
+        self.G = float(G_0)
         # initial fossil resource stock per household
         self.g_0 = float(G_0) / self.n
         # toggle resource depletion
@@ -106,37 +106,37 @@ class integrate_equations:
             assert len(x) == len(y)
 
             n = len(x)
-            cc = 0
+            ccc = 0
 
             for i in range(n):
                 for j in range(n):
-                    cc += x[i] * adj[i, j] * y[j]
+                    ccc += x[i] * adj[i, j] * y[j]
 
-            return float(cc)
+            return float(ccc)
 
-        cc = cl(adjacency, self.investment_decisions,
-                self.investment_decisions) / 2
-        dd = cl(adjacency, self.investment_decisions,
-                1 - self.investment_decisions)
-        cd = cl(adjacency, 1 - self.investment_decisions,
-                1 - self.investment_decisions) / 2
+        adj = adjacency
+        c = self.investment_decisions
+        d = - self.investment_decisions + 1
 
-        n = len(self.investment_decisions)
-        k = float(sum(sum(adjacency))) / 2
+        cc = cl(adj, c, c) / 2
+        cd = cl(adj, c, d)
+        dd = cl(adj, d, d) / 2
 
-        nc = sum(self.investment_decisions)
-        nd = sum(- self.investment_decisions + 1)
+        n = len(c)
+        k = float(sum(sum(adj))) / 2
+
+        nc = sum(c)
+        nd = sum(d)
 
         self.x = float(nc - nd) / n
         self.y = float(cc - dd) / k
         self.z = float(cd) / k
-        self.mucc = sum(investment_clean * self.investment_decisions) / nc
-        self.mucd = sum(investment_dirty * self.investment_decisions) / nc
-        self.mudc = sum(
-            investment_clean * (1 - self.investment_decisions)) / nd
-        self.mudd = sum(
-            investment_dirty * (1 - self.investment_decisions)) / nd
-        self.c = self.C / n
+
+        self.mucc = sum(investment_clean * c) / nc
+        self.mucd = sum(investment_dirty * c) / nc
+        self.mudc = sum(investment_clean * d) / nd
+        self.mudd = sum(investment_dirty * d) / nd
+        self.c = float(self.C) / n
         self.g = self.g_0
 
         self.k = float(k) / n
@@ -149,43 +149,57 @@ class integrate_equations:
         self.s_c, self.s_g = sp.symbols('c, g')
 
         # Define symbols for parameters
-        bc, bd, bR, e, delta, rs, xi, epsilon, phi, tau, pi, N, g0, k, p = \
-            sp.symbols(
-                'b_c b_d b_R e delta s xi epsilon phi tau, pi, N G_0 k P')
+        bc, bd, bR, e, delta, rs, xi, epsilon \
+            = sp.symbols('b_c b_d b_R e delta s xi epsilon')
+        phi, tau, pi, kappac, kappad, N, g0, k, p \
+            = sp.symbols(' phi tau pi kappa_c kappa_d N g_0 k p')
 
+        # Define lists of symbols and values for parameters to substitute
+        # in rhs expression
         param_symbols = [bc, bd, bR, e, delta, rs, xi,
-                         epsilon, phi, tau, pi, N, g0, k, p]
+                         epsilon, phi, tau, pi, kappac, kappad, N, g0, k, p]
         param_values = [self.b_c, self.b_d, self.b_r0, self.e, self.d_c,
                         self.s, self.xi, self.eps, self.phi, self.tau, self.pi,
-                        self.n, float(self.G_0) / self.n, self.k,
-                        float(self.p) / self.n]
-        self.columns = \
-            ['x', 'y', 'z', 'mucc', 'mucd', 'mudc', 'mudd', 'c', 'g']
+                        self.kappa_c, self.kappa_d,
+                        self.n,
+                        float(self.g_0), self.k,
+                        float(self.p)]
 
         # Load right hand side of ode system
         rhs = np.load(__file__.rsplit('/', 1)[0] + '/res_raw.pkl')
+        pcl = np.load(__file__.rsplit('/', 1)[0] + '/pcl.pkl')
 
         # substitute parameters into rhs and simplify once.
         self.rhs = rhs.subs({symbol: value for symbol, value
                              in zip(param_symbols, param_values)})
+        self.pcl = pcl.subs({symbol: value for symbol, value
+                             in zip(param_symbols, param_values)})
 
         # list to save macroscopic quantities to compare with
         # moment closure / pair based proxy approach
+        self.columns = \
+            ['x', 'y', 'z', 'mucc', 'mucd', 'mudc', 'mudd', 'c', 'g']
         self.m_trajectory = pd.DataFrame(columns=self.columns)
 
         # dictionary for final state
         self.final_state = {}
 
+    def _get_m_trj(self):
+
+        return self.m_trajectory
+
     def dot_rhs(self, values, t):
         var_symbols = [self.s_x, self.s_y, self.s_z, self.s_mucc,
                        self.s_mucd, self.s_mudc, self.s_mudd,
                        self.s_c, self.s_g]
-        if values[-1] < self.alpha * self.G_0:
-            values[-1] = self.alpha * self.G_0
+        if values[-1] < self.alpha * self.g_0:
+            values[-1] = self.alpha * self.g_0
         # add to g such that 1 - alpha**2 * (g/G_0)**2 remains positive
         subs1 = {var: val for (var, val) in zip(var_symbols, values)}
         rval = list(self.rhs.subs(subs1).evalf())
-        if self.R_depletion:
+        # print [rc, rd, Kc, Kd, Pc, Pd, R] for debugging
+        # print list(self.pcl.subs(subs1).evalf())
+        if not self.R_depletion:
             rval[-1] = 0
         return rval
 
@@ -194,15 +208,24 @@ class integrate_equations:
             t = np.linspace(self.t, t_max, 50)
             initial_conditions = [self.x, self.y, self.z, self.mucc,
                                   self.mucd, self.mudc, self.mudd,
-                                  self.c, self.G_0]
+                                  self.c, self.g]
             trajectory = odeint(self.dot_rhs, initial_conditions, t)
             df = pd.DataFrame(trajectory, index=t, columns=self.columns)
             self.m_trajectory = pd.concat([self.m_trajectory, df])
 
             # update aggregated variables:
+            self.x = trajectory[-1, 0]
+            self.y = trajectory[-1, 1]
+            self.z = trajectory[-1, 2]
+            self.mucc = trajectory[-1, 3]
+            self.mucd = trajectory[-1, 4]
+            self.mudc = trajectory[-1, 5]
+            self.mudd = trajectory[-1, 6]
+            self.c = trajectory[-1, 7]
+            self.g_0 = trajectory[-1, 8]
             self.C = self.c * self.n
             self.G = self.g * self.n
-
+            self.t = t_max
         elif t_max <= self.t:
             print 'upper time limit is smaller than system time', self.t
 
@@ -263,7 +286,7 @@ if __name__ == '__main__':
     init_conditions = (adjacency_matrix, opinions,
                        clean_investment, dirty_investment)
 
-    model = integrate_equations(*init_conditions, **input_parameters)
+    model = Integrate_Equations(*init_conditions, **input_parameters)
 
     model.run(t_max=2)
 

Implementation/micro_model/divestment_core.py
@@ -1,6 +1,7 @@
 
 import datetime
 import numpy as np
+import pandas as pd
 import networkx as nx
 from itertools import chain
 from scipy.integrate import odeint
@@ -9,14 +10,14 @@ from scipy.stats import linregress
 from random import shuffle
 
 
-class divestment_core:
+class Divestment_Core:
 
     def __init__(self, adjacency=None, opinions=None,
                  investment_clean=None, investment_dirty=None,
                  possible_opinions=None,
                  tau=0.8, phi=.7, eps=0.05,
-                 P=1000., r_b=0, b_c=1., b_d=1.5, s=0.23, d_c=0.06,
-                 b_r0=1., e=10, G_0=1000,
+                 P=100., r_b=0, b_c=1., b_d=1.5, s=0.23, d_c=0.06,
+                 b_r0=1., e=10, G_0=3000,
                  R_depletion=True, test=False,
                  C=1, beta=0.06, xi=1. / 8., learning=False,
                  campaign=False):
@@ -36,6 +37,7 @@ class divestment_core:
         # toggle e_trajectory output
         self.e_trajectory_output = True
         self.m_trajectory_output = True
+        self.switchlist_output = True
         # toggle whether to run full time or only until consensus
         self.run_full_time = True
         # toggle resource depletion
@@ -77,6 +79,8 @@ class divestment_core:
         # list to save macroscopic quantities to compare with
         # moment closure / pair based proxy approach
         self.m_trajectory = []
+        # list of data for switching events
+        self.switchlist = []
         # dictionary for final state
         self.final_state = {}
 
@@ -138,7 +142,10 @@ class divestment_core:
         # to keep track of investment decisions.
         self.decision_state = 0.
         # investment decision vector, so far equal to investment_decisions
-        self.investment_decisions = np.random.randint(0, 2, self.n)
+        if possible_opinions == [[0], [1]]:
+            self.investment_decisions = np.array(opinions)
+        else:
+            self.investment_decisions = np.random.randint(0, 2, self.n)
 
         # members of ALL household = population   
         self.P = P
@@ -242,6 +249,28 @@ class divestment_core:
         if self.m_trajectory_output:
             self.init_m_trajectory()
 
+    def _get_m_trj(self):
+        # make up Dataframe from macro data:
+        columns = self.m_trajectory.pop(0)
+        df = pd.DataFrame(self.m_trajectory, columns=columns)
+        df = df.set_index('time')
+
+        return df
+
+    def _get_e_trj(self):
+        # make up DataFrame from micro data
+        columns = self.e_trajectory.pop(0)
+        df = pd.DataFrame(self.e_trajectory, columns=columns)
+        df = df.set_index('time')
+
+        return df
+
+    def _get_switch_list(self):
+        columns = self.switchlist.pop(0)
+        df = pd.DataFrame(self.switchlist, columns=columns)
+        df = df.set_index('time')
+
+        return df
 
     def cue_0(self, i):
         """
@@ -549,19 +578,21 @@ class divestment_core:
 
         X_c = (self.b_c * C ** self.xi * K_c ** self.kappa_c) ** (
         1. / (1. - self.pi))
-        X_d = (self.b_d * K_d**self.kappa_d)**(1./(1.-self.pi))
-        X_R = (1. - b_R/self.e)**(1./(1.-self.pi))\
-            if 1 > b_R/self.e else float('NaN')
+        X_d = (self.b_d * K_d ** self.kappa_d) ** (1. / (1. - self.pi))
+        X_R = (1. - b_R / self.e) ** (1. / (1. - self.pi)) \
+            if 1 > b_R / self.e else float('NaN')
 
-        P_c = P*X_c/(X_c + X_d*X_R)
-        P_d = P*X_d*X_R/(X_c + X_d*X_R)
-        R = 1./self.e * self.b_d * K_d**self.kappa_c * P_d**self.pi
+        P_c = P * X_c / (X_c + X_d * X_R)
+        P_d = P * X_d * X_R / (X_c + X_d * X_R)
+        R = 1. / self.e * self.b_d * K_d ** self.kappa_d * P_d ** self.pi
 
-        self.w = self.pi * P**(self.pi - 1) * (X_c + X_d*X_R)**(1.-self.pi)
-        self.r_c = self.kappa_c /\
-            K_c * X_c * P**self.pi * (X_c + X_d*X_R)**(-self.pi)
-        self.r_d = self.kappa_d /\
-            K_d * X_d * X_R * P**self.pi * (X_c + X_d*X_R)**(-self.pi)
+        self.w = self.pi * P ** (self.pi - 1) * (X_c + X_d * X_R) ** (
+        1. - self.pi)
+        self.r_c = self.kappa_c / \
+                   K_c * X_c * P ** self.pi * (X_c + X_d * X_R) ** (- self.pi)
+        self.r_d = self.kappa_d / \
+                   K_d * X_d * X_R * P ** self.pi * (X_c + X_d * X_R) ** (
+        - self.pi)
 
         # check if dirty sector is profitable (P_d > 0).
         # if not, shut it down.
@@ -598,9 +629,11 @@ class divestment_core:
         P_dot = self.r_b * P
         C_dot = self.b_c * C ** self.xi * P_c ** self.pi \
                 * K_c ** self.kappa_c - C * self.beta if self.learning else 0.
-        investment_clean_dot = self.investment_decisions*self.s*self.income \
-            - self.investment_clean*self.d_c
-        investment_dirty_dot = np.logical_not(self.investment_decisions)\
+        investment_clean_dot = \
+            self.investment_decisions \
+            * self.s * self.income - self.investment_clean * self.d_c
+        investment_dirty_dot = \
+            np.logical_not(self.investment_decisions) \
             * self.s * self.income - self.investment_dirty*self.d_d
 
         x1 = np.fromiter(
@@ -608,7 +641,8 @@ class divestment_core:
                                  list(investment_dirty_dot),
                                  [P_dot, G_dot, C_dot]]),
             dtype='float')
-
+        # print for debugging:
+        # print [self.r_c, self.r_d, K_c, K_d, P_c, P_d, R]
         return x1
 
     def update_economy(self, update_time):
@@ -717,8 +751,12 @@ class divestment_core:
             # people trying new stuff at random
             rdn = np.random.uniform()
             if rdn < self.eps*(1-self.phi) and self.imitation:
-                self.opinions[candidate] = np.random.randint(
+                old_opinion = self.opinions[candidate]
+                new_opinion = np.random.randint(
                     len(self.possible_opinions))
+                self.opinions[candidate] = new_opinion
+                if old_opinion != new_opinion and self.switchlist_output:
+                    self.save_switch(candidate, old_opinion)
                 candidate = -1
                 break
             # noise in rewiring (sometimes they make new friends
@@ -801,11 +839,13 @@ class divestment_core:
             # if adapt
             # compare fitness
             df = self.fitness(neighbor) - self.fitness(candidate)
-            # and rewire, if not a campaigner
+            # and immitate, if not a campaigner
             if ((self.campaign is False
                     or opinion[candidate] != len(self.possible_opinions)-1)
                     and (np.random.uniform() < .5*(np.tanh(df) + 1))
                     and self.imitation):
+                if self.switchlist_output:
+                    self.save_switch(candidate, self.opinions[candidate])
                 self.opinions[candidate] = self.opinions[neighbor]
         return 0
 
@@ -978,8 +1018,8 @@ class divestment_core:
         pair based proxy.
         :return: None
         """
-        element = ['time', 'x', 'y', 'z', 'mucc', 'mucd', 'mudc', 'mudd', 'C',
-                   'G']
+        element = ['time', 'x', 'y', 'z', 'mucc', 'mucd', 'mudc', 'mudd', 'c',
+                   'g']
         self.m_trajectory.append(element)
 
         dt = [self.t, self.t]
@@ -1017,13 +1057,13 @@ class divestment_core:
             assert len(x) == len(y)
 
             n = len(x)
-            cc = 0
+            ccc = 0
 
             for i in range(n):
                 for j in range(n):
-                    cc += x[i] * adj[i, j] * y[j]
+                    ccc += x[i] * adj[i, j] * y[j]
 
-            return float(cc)
+            return float(ccc)
 
         adj = self.neighbors
         c = self.investment_decisions
@@ -1040,8 +1080,8 @@ class divestment_core:
         dd = cl(adj, d, d) / 2
 
         x = float(nc - nd) / n
-        y = (cc - dd) / k
-        z = cd / k
+        y = float(cc - dd) / k
+        z = float(cd) / k
 
         mucc = sum(self.investment_decisions * self.investment_clean) / nc
         mucd = sum(self.investment_decisions * self.investment_dirty) / nc
@@ -1050,8 +1090,18 @@ class divestment_core:
         mudd = sum((1 - self.investment_decisions)
                    * self.investment_dirty) / nd
 
-        entry = [self.t, x, y, z, mucc, mucd, mudc, mudd, self.C, self.G]
+        entry = [self.t, x, y, z, mucc, mucd, mudc, mudd, self.C / n,
+                 self.G / n]
         self.m_trajectory.append(entry)
+
+    def save_switch(self, i, direction):
+        if len(self.switchlist) == 0:
+            self.switchlist = [['time', '$K^{(c)}$', '$K^{(d)}$', 'direction']]
+        self.switchlist.append([self.t,
+                                self.investment_clean[i],
+                                self.investment_dirty[i],
+                                direction])
+
 
 if __name__ == '__main__':
     """
@@ -1066,9 +1116,9 @@ if __name__ == '__main__':
         + datetime.datetime.now().strftime("%d_%m_%H-%M-%Ss") + '_output'
 
     # Initial conditions:
-    Trigger = False
+    FFH = False
 
-    if Trigger:
+    if FFH:
 
         nopinions = [10, 10, 10, 10, 10, 10, 10, 10]
         possible_opinions = [[2, 3],  # short term investor
@@ -1079,25 +1129,25 @@ if __name__ == '__main__':
                              [4, 0],  # dirty conformer
                              [1],  # gutmensch
                              [0]]  # redneck
-        input_parameters = {'tau': 1, 'eps': 0.05, 'b_d': 1.2,
+        input_parameters = {'tau': 1, 'eps': 0.05, 'b_d': 1.4,
                             'b_c': 1., 'phi': 0.8, 'e': 100,
                             'G_0': 30000,
                             'possible_opinions': possible_opinions,
-                            'c': 100, 'xi': 1. / 8., 'beta': 0.03,
+                            'C': 100, 'xi': 1. / 8., 'beta': 0.03,
                             'campaign': False, 'learning': False}
 
-    if not Trigger:
+    if not FFH:
         # investment_decisions:
-        nopinions = [50, 50]
+        nopinions = [10, 10]
         possible_opinions = [[0], [1]]
 
         # Parameters:
 
-        input_parameters = {'tau': 1, 'eps': 0.05, 'b_d': 1.,
+        input_parameters = {'tau': 1, 'eps': 0.05, 'b_d': 1.2,
                             'b_c': 1., 'phi': 0.8, 'e': 100,
-                            'G_0': 30000, 'b_r0': 0.1 ** 2 * 100,
+                            'G_0': 1500, 'b_r0': 0.1 ** 2 * 100,
                             'possible_opinions': possible_opinions,
-                            'c': 1, 'xi': 1. / 8., 'beta': 0.06,
+                            'C': 1, 'xi': 1. / 8., 'beta': 0.06,
                             'campaign': False, 'learning': True}
 
     cops = ['c'+str(x) for x in possible_opinions]
@@ -1113,7 +1163,7 @@ if __name__ == '__main__':
 
     # network:
     N = sum(nopinions)
-    p = .2
+    p = 10. / N
 
     while True:
         net = nx.erdos_renyi_graph(N, p)
@@ -1134,20 +1184,20 @@ if __name__ == '__main__':
 
     # Initialize Model
 
-    model = divestment_core(*init_conditions,
+    model = Divestment_Core(*init_conditions,
                             **input_parameters)
 
     # Turn off economic trajectory
-    model.e_trajectory_output = False
+    model.e_trajectory_output = True
 
     # Turn on debugging
     model.debug = True
 
     # Run Model
     model.R_depletion = False
-    model.run(t_max=20)
+    # model.run(t_max=200)
     model.R_depletion = True
-    model.run(t_max=100)
+    model.run(t_max=600)
 
     # Print some output
 
@@ -1159,25 +1209,25 @@ if __name__ == '__main__':
     print 'finish time', model.t
     print 'steps computed', model.steps
 
-    trj = model.m_trajectory
-    headers = trj.pop(0)
-    df = pd.DataFrame(trj, columns=headers)
-    df = df.set_index('time')
-    print df[['x', 'y', 'z']]
+    colors = [c for c in 'gk']
+
+    df = model._get_e_trj()
+    print df.columns
 
     fig = mp.figure()
     ax1 = fig.add_subplot(221)
-    df[['x', 'y', 'z']].plot(ax=ax1)
+    df[['r_c', 'r_d']].plot(ax=ax1, style=colors)
 
     ax2 = fig.add_subplot(223)
-    df[['C']].plot(ax=ax2)
+    df[['wage']].plot(ax=ax2)
 
     ax3 = fig.add_subplot(224)
-    df[['G']].plot(ax=ax3)
+    df[['K_c', 'K_d']].plot(ax=ax3, style=colors)
 
     ax4 = fig.add_subplot(222)
-    df[['mucc', 'mucd', 'mudc', 'mudd']].plot(ax=ax4)
+    df[['G']].plot(ax=ax4, style=colors[1])
+    ax5 = ax4.twinx()
+    df[['C']].plot(ax=ax5, style=colors[0])
 
-    mp.show()
-
-
+    fig.tight_layout()
+    mp.savefig('example_trajectory.png')