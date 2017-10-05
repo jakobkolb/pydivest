85a8b3e0fd16865ca85d6b8082ec5f42dab90d9d
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2017-10-05 18:50:41 +0200

    now only pushing changes to aix

--- PyDivestment/cluster_tools/cluster_push.sh
+++ PyDivestment/cluster_tools/cluster_push.sh
@@ -1,1 +1,1 @@
-rsync -t -r --copy-links --exclude=divestdata * kolb@aix.pik-potsdam.de:/home/kolb/Divest_Experiments/ || rsync -t -r --copy-links --exclude=divestdata * kolb@cluster:/home/kolb/Divest_Experiments/
+rsync -t -r --progress --copy-links --exclude-from=cluster_tools/exclude.txt * kolb@aix.pik-potsdam.de:/home/kolb/Divest_Experiments/ 
