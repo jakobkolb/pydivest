8749ea09615aa0038bbbb7ada62831305de647ff
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2018-04-19 12:51:14 +0200

    updated setup.py dependencies plus minor changes to job scripts

--- PyDivestment/tests/test_X7.py
+++
@@ -1,8 +0,0 @@
-"""Test for X7 experiment."""
-
-from ..experiments import X7_compare_campaign_in_FFH_to_AV as X7
-
-for i in [0, 1]:
-    for j in [0, 1]:
-        for k in [0, 1]:
-            assert X7.run_experiment(['testing', 1, i, j, k]) == 1
