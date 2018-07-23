2a6511e449b0507b3bd1654dccc77cfbbbde95f7
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2018-07-23 15:16:15 +0200

    updated example run

--- PyDivestment/pydivest/macro_model/integrate_equations_rep.py
+++ PyDivestment/pydivest/macro_model/integrate_equations_rep.py
@@ -666,12 +666,12 @@ if __name__ == "__main__":
 
     input_parameters = {'i_tau': 1, 'eps': 0.05, 'b_d': 1.2,
                         'b_c': 1., 'i_phi': 0.8, 'e': 100,
-                        'G_0': 50, 'b_r0': 0.3 ** 2 * 100,
+                        'G_0': 800, 'b_r0': 0.3 ** 2 * 100,
                         'possible_cue_orders': possible_cue_orders,
                         'C': 100, 'xi': 1. / 8., 'd_c': 0.06,
                         'campaign': False, 'learning': True,
                         'crs': True, 'test': True,
-                        'R_depletion': False}
+                        'R_depletion': True}
 
     # investment_decisions
     opinions = []
@@ -701,11 +701,11 @@ if __name__ == "__main__":
 
     # m._setup_model()
 
-    m.run(t_max=50)
+    m.run(t_max=500)
 
-    m.R_depletion = True
-    m.set_parameters()
-    m.run(t_max=100)
+    #m.R_depletion = True
+    #m.set_parameters()
+    #m.run(t_max=100)
 
     # Plot the results
 
