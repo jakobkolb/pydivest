5c2af9040d040bd826d35f02218fb9a7e3561323
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2018-09-26 16:34:57 +0200

    testing all models (representative, aggregate, micro) in one test

--- PyDivestment/tests/run_model.py
+++ PyDivestment/tests/run_model.py
@@ -28,14 +28,15 @@ def run(name, model):
 
     # Parameters:
 
-    input_parameters = {'i_tau': 1, 'eps': 0.05, 'b_d': 1.2,
-                        'b_c': 1., 'i_phi': 0.8, 'e': 100,
-                        'G_0': 500, 'b_r0': 0.3 ** 2 * 100,
-                        'possible_cue_orders': possible_cue_orders,
-                        'C': 100, 'xi': 1. / 8., 'd_c': 0.06,
+    input_parameters = {'b_c': 1., 'phi': .4, 'tau': 1.,
+                        'eps': 0.05, 'b_d': 3.5, 'e': 1.,
+                        'b_r0': 0.2,
+                        'possible_cue_orders': [[0], [1]],
+                        'xi': 1. / 8., 'beta': 0.06,
+                        'L': 100., 'C': 1., 'G_0': 500000.,
                         'campaign': False, 'learning': True,
-                        'crs': True, 'test': True,
-                        'R_depletion': False}
+                        'interaction': 1, 'test': False,
+                        'R_depletion': True}
 
     # investment_decisions
     opinions = []
@@ -63,13 +64,7 @@ def run(name, model):
 
     m = model(*init_conditions, **input_parameters)
 
-    # m._setup_model()
-
-    m.run(t_max=100)
-
-    m.R_depletion = True
-    m.set_parameters()
-    m.run(t_max=350)
+    m.run(t_max=200)
 
     # Plot the results
 
