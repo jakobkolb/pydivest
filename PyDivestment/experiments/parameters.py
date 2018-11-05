5d3f4a2f2dce580db21cd89decd5106b04270959
Jakob J. Kolb <jakob.kolb@pik-potsdam.de>
2018-11-05 09:40:43 +0100

    updated experiment default parameters, such that resrouce depletes after approx. 200y

--- PyDivestment/experiments/parameters.py
+++ PyDivestment/experiments/parameters.py
@@ -6,7 +6,7 @@ class ExperimentDefaults:
                     'xi': .1, 'd_k': 0.06, 'd_c': 0.1,
                     'phi': .5, 'tau': 1., 'eps': 0.03,
                     'possible_cue_orders': [[0], [1]],
-                    'L': 100., 'C': 1., 'G_0': 500000.,
+                    'L': 100., 'C': 1., 'G_0': 1000000.,
                     'campaign': False, 'learning': True,
                     'interaction': 1, 'test': False,
                     'R_depletion': True}
